89f87591a8de73522238ac4a95afa099a3ab4aaa
Alan Panchenko <alnumberone@mail.ru.github.com>
2022-03-08 21:34:49 +0700

    Ispravil oshibki

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-int FF(const int size, char buff[], int i)
+int FF(const int MAX_MESSAGE_LENGTH, char MESSAGE[], int i)
 {
-    for (; i < size; i++) {
-        if (buff[i] == ' ') {
+    for (; i < MAX_MESSAGE_LENGTH; i++) {
+        if (MESSAGE[i] == ' ') {
             continue;
         } else {
             break;
@@ -15,34 +15,34 @@ int FF(const int size, char buff[], int i)
 }
 int main()
 {
-    const int size = 30;
-    char buff[size];
-    fgets(buff, size, stdin);
+    const int MAX_MESSAGE_LENGTH = 30;
+    char MESSAGE[MAX_MESSAGE_LENGTH];
+    fgets(MESSAGE, MAX_MESSAGE_LENGTH, stdin);
     int i = 0;
-    i = FF(size, buff, i);
-    if (i >= size) {
-        printf("oshibka");
+    i = FF(MAX_MESSAGE_LENGTH, MESSAGE, i);
+    if (i >= MAX_MESSAGE_LENGTH) {
+        printf("Volume exceeded");
         return 1;
     }
     const char circle[] = "circle";
     for (int g = 0; g < 6; g++, i++) {
-        if (tolower(buff[i]) == circle[g]) {
+        if (tolower(MESSAGE[i]) == circle[g]) {
             continue;
         } else {
-            printf("oshibka");
-            return 1;
+            printf("Wrong text input");
+            return 2;
         }
     }
-    i = FF(size, buff, i);
-    if (i >= size) {
-        printf("oshibka");
+    i = FF(MAX_MESSAGE_LENGTH, MESSAGE, i);
+    if (i >= MAX_MESSAGE_LENGTH) {
+        printf("Volume exceeded");
         return 1;
     }
     float x, y, r;
-    if (3 == sscanf(buff + i, "(%f %f, %f)\n", &x, &y, &r)) {
+    if (3 == sscanf(MESSAGE + i, "(%f %f, %f)\n", &x, &y, &r)) {
         printf(" x = %f y = %f R = %f\n", x, y, r);
-        printf("Circle\n");
+        printf("%s\n", circle);
     } else {
-        printf("oshibka");
+        printf("Incorrect input of coordinates");
     }
 }
