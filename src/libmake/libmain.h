ccfccce140e16158e19ebd91694d9de44c94000c
ALAN PANCHENKO <alanumberone@mail.ru>
2022-05-15 22:01:47 +0700

    Fix libmain.h

--- src/libmake/libmain.h
+++ src/libmake/libmain.h
@@ -1,8 +1,8 @@
 #pragma once
 
 int FF(const int MAX_MESSAGE_LENGTH, char MESSAGE[], int i);
-float S(float* );
-float P(float* );
-float S1(float* );
-float P1(float* );
+float S(float*);
+float P(float*);
+float S1(float*);
+float P1(float*);
 void intersect(int j, int c, float xp[], float yp[], float radius[]);
