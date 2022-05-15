d72d2b7a47c483ecb1f0447674baaf3509b9a026
ALAN PANCHENKO <alanumberone@mail.ru>
2022-05-15 21:45:37 +0700

    Fix libmain.c

--- src/libmake/libmain.c
+++ src/libmake/libmain.c
@@ -1,9 +1,8 @@
-#include <stdio.h>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string.h>
 #include "libmain.h"
 #include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+float xp[1], yp[1], radius[1];
 int FF(const int MAX_MESSAGE_LENGTH, char MESSAGE[], int i)
 {
     for (; i < MAX_MESSAGE_LENGTH; i++) {
@@ -16,15 +15,53 @@ int FF(const int MAX_MESSAGE_LENGTH, char MESSAGE[], int i)
     return i;
 }
 
-float S(float r){
-	float S;
-	S = M_PI * r * r;
-	return S;
+float S(float radius[])
+{
+    float S;
+    float r = radius[0];
+    S = M_PI * r * r;
+    return S;
 }
 
-float P(float r){
-	float P;
-	P = 2 * M_PI * r;
-	return P;
+float P(float radius[])
+{
+    float P;
+    float r = radius[0];
+    P = 2 * M_PI * r;
+    return P;
 }
 
+float S1(float radius[])
+{
+    float S1;
+    float r = radius[1];
+    S1 = M_PI * r * r;
+    return S1;
+}
+
+float P1(float radius[])
+{
+    float P1;
+    float r = radius[1];
+    P1 = 2 * M_PI * r;
+    return P1;
+}
+
+void intersect(int j, int c, float xp[], float yp[], float radius[])
+{
+    float d;
+    int g = 0;
+    char figure[] = "circle";
+    for (int k = 0; k <= c; k++) {
+        if (k == j)
+            continue;
+        d = fabs(sqrt(pow((xp[k] - xp[j]), 2) + pow((yp[k] - yp[j]), 2)));
+        if (d <= (radius[j] + radius[k]))
+            g++;
+    }
+    if (g > 0) {
+        printf("\nintersect: ");
+        for (int i = 0; i < g; i++)
+            printf("\n%s\n", figure);
+    }
+}
